276eb73db1a7258609f14560699915a0b96bc5a7
Deepak <85413425+anopenprogrammer@users.noreply.github.com>
2023-12-28 12:04:15 +0530

    Adding Redux Todo

---
+++ reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,14 @@
+import AddTodos from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+ 
+  return (
+    <>
+      <AddTodos />
+      <Todos />
+    </>
+  )
+}
+
+export default App
